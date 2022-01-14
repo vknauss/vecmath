96c3be00b6348dffd35a6dc834efee8084348b98
Vincent Knauss <knaus039@umn.edu>
2022-01-14 10:42:51 -0600

    changed cmake interface include to "include" instead of "include/vvm", meaning dep. projects will need to e.g. "#include <vvm/vvm.hpp>", to make things cleaner. updated test program.

--- test/main.cpp
+++ test/main.cpp
@@ -1,83 +1,38 @@
-#include <cstdio>
+#include <iostream>
 
-#include "matrix_ops.hpp"
-#include "vvm.hpp"
-#include "matrix_tfm.hpp"
+#include <vvm/vvm.hpp>
+#include <vvm/matrix_tfm.hpp>
+#include <vvm/string.hpp>
+
 
 using namespace vvm;
 
-template<typename T>
-void print(const v2<T>& v) {
-    printf("v2(%.4f, %.4f)\n",
-        v.x, v.y);
-}
-
-template<typename T>
-void print(const v3<T>& v) {
-    printf("v3(%.4f, %.4f, %.4f)\n",
-        v.x, v.y, v.z);
-}
-
-template<typename T>
-void print(const v4<T>& v) {
-    printf("v4(%.4f, %.4f, %.4f, %.4f)\n",
-        v.x, v.y, v.z, v.w);
-}
-
-template<typename T>
-void print(const m2<T>& m) {
-    printf("m2((%.4f, %.4f),\n\
-    (%.4f, %.4f))\n",
-        m[0][0], m[0][1],
-        m[1][0], m[1][1]);
-}
-
-template<typename T>
-void print(const m3<T>& m) {
-    printf("m3((%.4f, %.4f, %.4f),\n\
-    (%.4f, %.4f, %.4f),\n\
-    (%.4f, %.4f, %.4f))\n",
-        m[0][0], m[0][1], m[0][2],
-        m[1][0], m[1][1], m[1][2],
-        m[2][0], m[2][1], m[2][2]);
-}
-
-template<typename T>
-void print(const m4<T>& m) {
-    printf("m4((%.4f, %.4f, %.4f, %.4f),\n\
-    (%.4f, %.4f, %.4f, %.4f),\n\
-    (%.4f, %.4f, %.4f, %.4f),\n\
-    (%.4f, %.4f, %.4f, %.4f))\n",
-        m[0][0], m[0][1], m[0][2], m[0][3],
-        m[1][0], m[1][1], m[1][2], m[1][3],
-        m[2][0], m[2][1], m[2][2], m[2][3],
-        m[3][0], m[3][1], m[3][2], m[3][3]);
-}
-
 int main() {
     
-    printf("Test printing vectors.\n");
-    print(v2f(0, 1));
-    print(v3f(2, 3, 4));
-    print(v4f(5, 6, 7, 8));
+    std::cout << "Test printing vectors.\n";
+    std::cout << vvm::to_string(v2f(0, 1)) << "\n";
+    std::cout << vvm::to_string(v3f(2, 3, 4)) << "\n";
+    std::cout << vvm::to_string(v4f(5, 6, 7, 8)) << "\n";
 
-    printf("Test printing matrices.\n");
-    print(m2f(0, 1, 2, 3));
-    print(m3f(1, 2, 3, 4, 5, 6, 7, 8, 9));
-    print(m4f(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16));
+    std::cout << "Test printing matrices.\n";
+    std::cout << vvm::to_string(m2f(0, 1, 2, 3)) << "\n";
+    std::cout << vvm::to_string(m3f(1, 2, 3, 4, 5, 6, 7, 8, 9)) << "\n";
+    std::cout << vvm::to_string(m4f(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)) << "\n";
 
-    printf("Test printing matrices constructed from columns.\n");
-    print(m2f({0, 1}, {2, 3}));
-    print(m3f({1, 2, 3}, {4, 5, 6}, {7, 8, 9}));
-    print(m4f({1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}));
+    std::cout << "Test printing matrices constructed from columns.\n";
+    std::cout << vvm::to_string(m2f({0, 1}, {2, 3})) << "\n";
+    std::cout << vvm::to_string(m3f({1, 2, 3}, {4, 5, 6}, {7, 8, 9})) << "\n";
+    std::cout << vvm::to_string(m4f({1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16})) << "\n";
 
-    printf("Test identity matrices.\n");
-    print(identity<m2f>());
-    print(identity<m3f>());
-    print(identity<m4f>());
+    std::cout << "Test identity matrices.\n";
+    std::cout << vvm::to_string(identity<m2f>()) << "\n";
+    std::cout << vvm::to_string(identity<m3f>()) << "\n";
+    std::cout << vvm::to_string(identity<m4f>()) << "\n";
 
-    printf("Test transformations.\n");
-    print(translate(v3d(1, 2, 3)));
+    std::cout << "Test transformations.\n";
+    std::cout << vvm::to_string(translate(v3d(1, 2, 3))) << "\n";
+
+    std::cout << std::endl;
 
     return 0;
 }
